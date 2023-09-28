5023259810e54b59d722b36f1bcb4a6aec43e4e5
hinga1 <122677989+hinga1@users.noreply.github.com>
2023-09-29 00:24:32 +0300

    added more html and css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -17,3 +17,32 @@
 
   // Last Modified Placeholder
   document.getElementById("lastModified").innerHTML = "Last modified: " + document.lastModified;
+
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const navigationMenu = document.getElementById('menu-items');
+
+  // Add a click event listener to the hamburger menu button
+  hamburgerMenu.addEventListener('click', () => {
+      // Toggle the "clicked" class on the hamburger menu button
+      hamburgerMenu.classList.toggle('clicked');
+      // Toggle the visibility of the navigation menu
+      navigationMenu.style.display = navigationMenu.style.display === 'flex' ? 'none' : 'flex';
+  });
+
+// Get references to the menu button and menu items
+const menuButton = document.getElementById("hamburger-menu");
+const menuItems = document.getElementById("menu-items");
+
+// Add an event listener to the menu button
+menuButton.addEventListener("click", () => {
+    // Toggle the "hide-menu" class on the menu items
+    menuItems.classList.toggle("hide-menu");
+
+    // Toggle the hamburger icon to "X" and vice versa
+    if (menuButton.textContent === "✕") {
+        menuButton.textContent = "☰";
+    } else {
+        menuButton.textContent = "✕";
+    }
+});
+
