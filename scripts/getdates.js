7c7cbe71216685ae771fc297f44dd3d724609e5c
hinga1 <122677989+hinga1@users.noreply.github.com>
2023-09-29 00:42:49 +0300

    added javascript and css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,48 +1,29 @@
+// Weather Placeholder
+document.getElementById("weather").innerHTML = "Current weather: Sunny";
 
-    // Weather Placeholder
-    document.getElementById("weather").innerHTML = "Current weather: Sunny";
+// Number of Visits Placeholder
+if (localStorage.getItem("visitCount")) {
+  var count = parseInt(localStorage.getItem("visitCount"));
+  count++;
+  localStorage.setItem("visitCount", count);
+} else {
+  localStorage.setItem("visitCount", 1);
+}
+document.getElementById("visitCount").innerHTML = localStorage.getItem("visitCount");
 
-    // Number of Visits Placeholder
-    if (localStorage.getItem("visitCount")) {
-      var count = parseInt(localStorage.getItem("visitCount"));
-      count++;
-      localStorage.setItem("visitCount", count);
-    } else {
-      localStorage.setItem("visitCount", 1);
-    }
-    document.getElementById("visitCount").innerHTML = localStorage.getItem("visitCount");
+// Current Year Placeholder
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
-    // Current Year Placeholder
-  document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+// Last Modified Placeholder
+document.getElementById("lastModified").innerHTML = "Last modified: " + document.lastModified;
 
-  // Last Modified Placeholder
-  document.getElementById("lastModified").innerHTML = "Last modified: " + document.lastModified;
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const navigationMenu = document.getElementById('menu-items');
 
-  const hamburgerMenu = document.getElementById('hamburger-menu');
-  const navigationMenu = document.getElementById('menu-items');
-
-  // Add a click event listener to the hamburger menu button
-  hamburgerMenu.addEventListener('click', () => {
-      // Toggle the "clicked" class on the hamburger menu button
-      hamburgerMenu.classList.toggle('clicked');
-      // Toggle the visibility of the navigation menu
-      navigationMenu.style.display = navigationMenu.style.display === 'flex' ? 'none' : 'flex';
-  });
-
-// Get references to the menu button and menu items
-const menuButton = document.getElementById("hamburger-menu");
-const menuItems = document.getElementById("menu-items");
-
-// Add an event listener to the menu button
-menuButton.addEventListener("click", () => {
-    // Toggle the "hide-menu" class on the menu items
-    menuItems.classList.toggle("hide-menu");
-
-    // Toggle the hamburger icon to "X" and vice versa
-    if (menuButton.textContent === "✕") {
-        menuButton.textContent = "☰";
-    } else {
-        menuButton.textContent = "✕";
-    }
+// Add a click event listener to the hamburger menu button
+hamburgerMenu.addEventListener('click', () => {
+    // Toggle the "clicked" class on the hamburger menu button
+    hamburgerMenu.classList.toggle('clicked');
+    // Toggle the visibility of the navigation menu
+    navigationMenu.style.display = navigationMenu.style.display === 'flex' ? 'none' : 'flex';
 });
-
