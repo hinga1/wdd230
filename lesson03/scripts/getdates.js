f9c3b1bf89fe68ad6095da83208c446ed4d8395d
hinga1 <122677989+hinga1@users.noreply.github.com>
2023-09-30 20:04:33 +0300

    added site plan and css

---
+++ lesson03/scripts/getdates.js
@@ -0,0 +1,4 @@
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+// Last Modified Placeholder
+document.getElementById("lastModified").innerHTML = "Last modified: " + document.lastModified;
